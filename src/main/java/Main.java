8c2194b5ddf8e62f9d8e31f14de04cb125b3f512
Drobot Andriy <da.iuvenis@gmail.com>
2022-08-22 17:23:04 +0300

    Deleted unused lines

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,7 @@
-import enums.MonthUtils;
-import enums.Months;
 import map.Account;
 import map.Person;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class Main {
-    private static final Logger LOGGER = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
 //       new ListUtils()
@@ -16,8 +11,8 @@ public class Main {
 //                .getSortedHashMap();
 
 //        Account account = new Account();
-//        account.createAccount(new Person(1),new Account("andrii","asddsa"));
-//        account.createAccount(new Person(2),new Account("vlad","asdgas"));
+//        account.createAccount(new Person(1), new Account("andrii", "asddsa"));
+//        account.createAccount(new Person(2), new Account("vlad", "asdgas"));
 //        account.printAllAccounts()
 //                .deleteAccount(1).deletePerson(2)
 //                .printAllAccounts();
