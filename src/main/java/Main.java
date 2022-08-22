aaaa49cd866956fc3cef8e4d69816bfb7c0ee98a
Drobot Andriy <da.iuvenis@gmail.com>
2022-08-22 15:10:05 +0300

    Created methods for adding, printing and deleting Person and Account

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import map.Account;
+import map.Person;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,7 +10,14 @@ public class Main {
 //       new ListUtils()
 //               .collectToArray()
 //               .chooseOption();
-        new UtilsSet()
-                .getSortedHashMap();
+//        new UtilsSet()
+//                .getSortedHashMap();
+
+        Account account = new Account();
+        account.createAccount(new Person(1),new Account("andrii","asddsa"));
+        account.createAccount(new Person(2),new Account("vlad","asdgas"));
+        account.printAllAccounts();
+        account.deleteAccount(1).deletePerson(2);
+        account.printAllAccounts();
     }
 }
---
+++ src/main/java/map/Person.java
@@ -0,0 +1,55 @@
+package map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static map.Account.accountMap;
+import static map.Account.personKeys;
+
+public class Person {
+    private int keyId;
+    private static final Logger LOGGER = LogManager.getLogger(Person.class);
+
+
+    public int getKeyId() {
+        return keyId;
+    }
+
+    public Person() {
+    }
+
+    public Person(int keyId) {
+        this.keyId = keyId;
+    }
+
+    public Person deletePerson(int personKey) {
+        personKeys = accountMap.keySet();
+        List<Person> toRemove = new ArrayList<>();
+        try {
+            for (Person i : personKeys) {
+                if (i.getKeyId() == personKey) toRemove.add(i);
+            }
+            try {
+                for (int i = 0; i < toRemove.size(); i++) {
+                    accountMap.remove(toRemove.get(i));
+                    LOGGER.info("Persons were been deleted");
+
+                }
+            } catch (Exception e) {
+                LOGGER.error(e);
+            }
+        } catch (Exception e) {
+            LOGGER.error(e);
+        }
+
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "keyId=" + keyId;
+    }
+}
